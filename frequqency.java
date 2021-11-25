326a734167131852ab3b3eb5b64e88f90b48f2f2
KARTHIK7981 <81958618+KARTHIK7981@users.noreply.github.com>
2021-11-25 21:52:01 +0530

    Update frequqency.java

--- frequqency.java
+++ frequqency.java
@@ -1,6 +1,5 @@
-package 
 public class Frequency {  
-    public static void main(String[] args) { 
+public static void main(String[] args) { 
         Scanner in=new Scanner(System.in);
         System.out.println("Enter size of array");
         int size=in.nextInt();
