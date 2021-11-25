90d87a144f8afb6e0c7dca85c4423e2cf75acae6
KARTHIK7981 <81958618+KARTHIK7981@users.noreply.github.com>
2021-11-25 16:13:29 +0530

    Add files via upload

---
+++ linearsearch.java
@@ -0,0 +1,15 @@
+public class linearsearch{
+    public static intlinearsearch(int [] arr,int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int a1[]={1,2,3,4,5,6};
+        int key=5;
+        System.out.println(key"linearsearch"a1(a1,key));
+    }
+}
